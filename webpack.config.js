57d1d4809b0484173b8b4895d7a7ac6f71f3ed16
okhun <okhunjongaybullaev@gmail.com>
2021-06-10 20:43:37 +0500

    match game commit

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path=require('path');
+
+module.exports={
+    entry:'./src/index.ts',
+    output:{
+        path: path.resolve(__dirname,'dist'),
+        filename:'bundle.js',
+        publicPath:"/dist/"
+    },
+    module:{
+        rules:[
+            {
+                test:/\.ts$/,
+                exclude:/node_modules/,
+                use:{
+                    loader:"ts-loader"
+                }
+            }
+        ]
+    },
+    resolve:{
+        extensions:['.ts']
+    }
+}
