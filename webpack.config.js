08b8883c6357d3b67132f858c9b5cab014cb07e6
okhun <okhunjongaybullaev@gmail.com>
2021-06-24 18:52:00 +0500

    new build

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,25 @@
-const path=require('path');
+const path = require("path");
 
-module.exports={
-    entry:'./src/index.ts',
-    output:{
-        path: path.resolve(__dirname,'dist'),
-        filename:'bundle.js',
-        publicPath:"/dist/"
-    },
-    module:{
-        rules:[
-            {
-                test:/\.ts$/,
-                exclude:/node_modules/,
-                use:{
-                    loader:"ts-loader"
-                }
-            }
-        ]
-    },
-    resolve:{
-        extensions:['.ts']
-    }
-}
+module.exports = {
+  entry: "./src/index.ts",
+  devtool: "inline-source-map",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/dist/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "ts-loader",
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+};
